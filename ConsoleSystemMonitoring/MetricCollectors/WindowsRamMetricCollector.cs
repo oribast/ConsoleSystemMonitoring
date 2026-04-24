f9c63c531b0cf270b36469ea1036546cc00b3fc2
Dmitry Pichuzhkin <pichuzhkin.dmitry@outlook.com>
2026-04-24 16:54:18 +0300

    Implemented ram metric collector

---
+++ ConsoleSystemMonitoring/MetricCollectors/WindowsRamMetricCollector.cs
@@ -0,0 +1,26 @@
+﻿using System.Text;
+using Hardware.Info;
+
+namespace ConsoleSystemMonitoring.MetricCollectors
+{
+    internal class WindowsRamMetricCollector: BaseMetricCollector
+    {
+        private readonly HardwareInfo info = new();
+
+        public override string GetStringData()
+        {
+            info.RefreshMemoryStatus();
+            var resultString = new StringBuilder();
+
+            resultString.AppendLine($"Total RAM: {GetTotalRamValue() / 1024 / 1024} MB");
+            resultString.AppendLine($"Available RAM: {GetAvailableRamValue() / 1024 / 1024} MB");
+            resultString.AppendLine($"RAM Usage: {GetRamUsagePercent():F2}%");
+
+            return resultString.ToString();
+        }
+
+        private ulong GetTotalRamValue() => info.MemoryStatus.TotalPhysical;
+        private ulong GetAvailableRamValue() => info.MemoryStatus.AvailablePhysical;
+        private double GetRamUsagePercent() => (double)(GetTotalRamValue() - GetAvailableRamValue()) / GetTotalRamValue() * 100;
+    }
+}
--- ConsoleSystemMonitoring/Program.cs
+++ ConsoleSystemMonitoring/Program.cs
@@ -11,6 +11,16 @@ namespace ConsoleSystemMonitoring
                 Console.WriteLine("This application is only supported on Windows.");
                 return;
             }
+
+            //var cpuCollector = new MetricCollectors.WindowsCpuMetricCollector();
+            //var ramCollector = new MetricCollectors.WindowsRamMetricCollector();
+            //while (true)
+            //{
+            //    Console.WriteLine(cpuCollector.GetStringData());
+            //    Console.WriteLine(ramCollector.GetStringData());
+            //    Thread.Sleep(1000);
+            //    Console.Clear();
+            //}
         }
     }
 }
